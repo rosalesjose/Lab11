c73b2e55f9226a5385fb14839c1f5a3b7afb7d8e
Jose <jrosales0001@gmail.com>
2018-02-05 14:22:48 -0500

    list

--- Lab11/Program.cs
+++ Lab11/Program.cs
@@ -10,14 +10,17 @@ namespace Lab11
     {
         static void Main(string[] args)
         {
-            Person p1 = new Person("John", "123 Sesame St.");
-            Console.WriteLine(p1.ToString());
+            Console.Title = "Lab 11";
 
-            Student student1 = new Student("Cass", "123 Sunset Blvd.", "Geography", 4, 52000);
-            Console.WriteLine(student1.ToString());
+            List<Person> Plist = new List<Person>();
+            Plist.Add(new Person("John", "123 Sesame St."));
+            Plist.Add(new Student("Cass", "123 Sunset Blvd.", "Geography", 4, 52000));
+            Plist.Add(new Staff("Caesar", "123 Broadway Rd.", "Parsons", 89000.00));
 
-            Staff staff1 = new Staff("Caesar", "123 Broadway Rd.", "Parsons", 89000.00);
-            Console.WriteLine(staff1.ToString());
+            foreach (Person o in Plist)
+            {
+                Console.WriteLine(o.ToString());
+            }    
         }
     }
 }
