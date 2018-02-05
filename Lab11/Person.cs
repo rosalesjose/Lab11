0af055d7638128a7e2fe8888abb923189f657395
Jose <jrosales0001@gmail.com>
2018-02-05 13:28:23 -0500

    classes

---
+++ Lab11/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Person
+    {
+        private string name;
+        private string address;
+
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+
+        public string Address
+        {
+            set { address = value; }
+            get { return address; }
+        }
+
+        public Person(string name, string address)
+        {
+            this.name = name;
+            this.address = address;
+        }
+    }
+}
---
+++ Lab11/Staff.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Staff : Person
+    {
+        private string school;
+        private double pay;
+
+        public string School
+        {
+            set { school = value; }
+            get { return school; }
+        }
+
+        public double Pay
+        {
+            set { pay = value; }
+            get { return pay; }
+        }
+
+        public Staff(string name, string address, string school, double pay) : base(name, address)
+        {
+            this.school = school;
+            this.pay = pay;
+        }
+    }
+}
